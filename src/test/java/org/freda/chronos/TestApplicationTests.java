729a6423535d478700a229c85cce62507bfd37d9
Rally <zhangyu4564@sina.com>
2021-03-16 22:53:45 +0800

    feature: init project.

---
+++ src/test/java/org/freda/chronos/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.freda.chronos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
